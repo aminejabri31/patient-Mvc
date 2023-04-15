e0996c4e111fc994a0d9faf5e9eabbc2e80dcca9
aminejabri31 <73138380+aminejabri31@users.noreply.github.com>
2023-04-15 20:47:55 +0000

    First commit

---
+++ src/main/java/ma/emsi/patientmvc/PatientMvcApplication.java
@@ -0,0 +1,35 @@
+package ma.emsi.patientmvc;
+
+import ma.emsi.patientmvc.entities.Patient;
+import ma.emsi.patientmvc.repositories.PatientRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.CommandLineRunner;
+
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PatientMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientMvcApplication.class, args);
+    }
+
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
+        return args -> {
+            patientRepository.save(new Patient(null, "amine", new Date(), false, 12));
+            patientRepository.save(new Patient(null, "hassan", new Date(), true, 20));
+            patientRepository.save(new Patient(null, "mehdi", new Date(), false, 51));
+            patientRepository.save(new Patient(null, "yassine", new Date(), true, 190));
+            patientRepository.findAll().forEach(
+                    p -> {
+                        System.out.println(p.getNom());
+                    }
+            );
+
+        };
+    }
+}
